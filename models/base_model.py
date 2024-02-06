9ebad5d18b72697e36d5f552687c694e70918c28
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-02-06 13:44:36 -0800

    Updated base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,6 @@
 
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
@@ -11,9 +10,20 @@ class BaseModel:
 
     def __init__(self):
         """Initializing the BaseModel instance"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = datetime.strptime(
+                        kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if 'update_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(
+                        kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns the string representation of the BaseModel instance"""
