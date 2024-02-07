2f290915eeb85deb15a0899d6aa0bedf5be57fa5
brikhotso <brenda.rikhotso@gmail.com>
2024-02-07 13:17:13 -0800

    console 0.0.1

--- console.py
+++ console.py
@@ -1,3 +1,25 @@
 #!/usr/bin/python3
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
+
+class HBNBCommand(cmd.Cmd):
+    """Define HBNB command interpreter"""
+    prompt = '(hbnb)'
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered."""
+        pass
+
+    def do_quit(self, line):
+        """Exit the program. Usage: quit"""
+        return True
+
+    def do_EOF(self, line):
+        """Exit the program. Usage: Ctrl-D"""
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
