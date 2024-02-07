593bc27b82a8619388fb56f3551939525c5abd5e
brikhotso <brenda.rikhotso@gmail.com>
2024-02-07 13:21:08 -0800

    created city model

---
+++ models/city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Contains class city"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City class inheriting from BaseModel"""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes City instance
+
+        Args:
+            *args: Tuple of class instance arguments
+            **kwargs: Dictionary of class instance arguments
+        """
+        super().__init__(*args, **kwargs)
+        self.state_id = kwargs.get('state_id', '')
+        self.name = kwargs.get('name', '')
