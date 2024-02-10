a488fb62de0ff2fe074f72c6106a0e9915be072f
brikhotso <brenda.rikhotso@gmail.com>
2024-02-10 06:02:42 -0800

    user class update

--- models/user.py
+++ models/user.py
@@ -5,17 +5,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """User class inheriting from BaseModel"""
-
-    def __init__(self, *args, **kwargs):
-        """
-        Initializes User instance
-
-        Args:
-            *args: Tuple of class instance arguments
-            **kwargs: Dictionary of class instance arguments
-        """
-        super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    self.email = ""
+    self.password = ""
+    self.first_name = ""
+    self.last_name = ""
