65adbd2bfdc205b90be5007d759eb2cc6d842c74
brikhotso <brenda.rikhotso@gmail.com>
2024-02-10 08:15:51 -0800

    update file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-""" contains file storage and related methods"""
+""" Contains file storage"""
 import json
 from models.base_model import BaseModel
 from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 
 class FileStorage:
