3c33eec4ee1fa97021b934dd7b7c7e9146cd1e04
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-02-10 13:22:11 -0800

    Pycodestyling

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Contains file storage"""
+"""Contains file storage"""
 
 import json
 from models.base_model import BaseModel
@@ -15,7 +15,6 @@ class FileStorage:
     """
     Class for serializing and deserializing objects to and from a JSON file.
     """
-
     __file_path = "file.json"
     __objects = {}
 
