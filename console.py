35a40d012952be1dc0aa5379a7999c082ec387eb
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-02-09 20:19:53 -0800

    Updating console - update

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import cmd
+import shlex
 from models.base_model import BaseModel
 from models import storage
 
@@ -111,7 +112,48 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
             return
 
-        print([str(instance) for key, instance in storage.all().items() if arg in key])
+        print([str(instance) for key, instance in
+               storage.all().items() if arg in key])
+
+    def do_update(self, arg):
+        """Updates an instance based on class name and id"""
+
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = shlex.split(arg)
+        if len(args) < 1:
+            print("** instance id missing **")
+            return
+
+        try:
+            cls = eval(args[0])
+        except NameError:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance = storage.all()
+        key = f"{args[0]}.{args[1]}"
+        if key not in instance:
+            print("** no instance found **")
+            return
+
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        inst = instance[key]
+        setattr(inst, args[2], args[3])
+        inst.save()
 
 
 if __name__ == '__main__':
